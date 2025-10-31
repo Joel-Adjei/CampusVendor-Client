8797b6753a6c271ff5dbeedfba1d7b029dc02060
JoelAdj01 <kofienergy15@gmail.com>
2025-10-31 20:46:38 +0000

    Signup update

--- frontend/src/components/ui/Modal.jsx
+++ frontend/src/components/ui/Modal.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Modal =({ display , children})=>{
+    useEffect(() => {
+        if (display) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = "";
+        }
+    
+        // Clean up just in case
+        return () => {
+          document.body.style.overflow = "";
+        };
+      }, [display]);
+
     if(!display) return null;
 
     return(
         <section className="fixed flex justify-center items-center top-0 left-0 z-50 w-full h-[100vh] bg-black/50 backdrop-blur-xs p-4">
-            <div className="bg-white rounded-2xl shadow-2xl max-w-4xl w-full max-h-[90vh] ">
+            <div className="custom-scrollbar bg-white rounded-2xl shadow-2xl max-w-4xl w-full max-h-[90vh] overflow-y-auto ">
                 {children}
             </div>
         </section>
--- frontend/src/components/ui/custom/Button.jsx
+++ frontend/src/components/ui/custom/Button.jsx
@@ -1,53 +1,72 @@
 import { Loader2, LoaderIcon } from "lucide-react";
-import React from "react"
+import React from "react";
 
-const Button =({children , iconStyle , onClick , disabled = false , type , isLoading = false , variant = "primary" , iconType="icon-left" , Icon , className})=>{
+const Button = ({
+  children,
+  iconSize,
+  iconStyle,
+  onClick,
+  disabled = false,
+  type,
+  isLoading = false,
+  variant = "primary",
+  iconType = "icon-left",
+  Icon,
+  className,
+}) => {
+  function getColors() {
+    switch (variant) {
+      case "primary" || "":
+        return "bg-gradient-to-br from-blue-900 to-blue-400 text-white cursor-pointer";
 
-    function getColors(){
-        switch (variant) {
-            case ("primary" || ""):
-                return "bg-gradient-to-br from-blue-900 to-blue-400 text-white cursor-pointer";
-            
-            case "secondary":
-                return "bg-gradient-to-br from-blue-100 to-yellow-100 text-blue-950 shadow-lg cursor-pointer"
-            
-            case "outline":
-                return "bg-none border-1 border-blue-700 text-blue-700 cursor-pointer"
-        
-            default:
-                return "bg-gray-400 text-gray-300 cursor-not-allowed";
-        }
+      case "secondary":
+        return "bg-gradient-to-br from-blue-100 to-yellow-100 text-blue-950 shadow-lg cursor-pointer";
+
+      case "outline":
+        return "bg-none border-1 border-blue-700 text-blue-700 cursor-pointer";
+
+      default:
+        return "bg-gray-400 text-gray-300 cursor-not-allowed";
     }
+  }
 
-    return(
-        <>
-        
-        {!(iconType == "icon-only" )? 
+  return (
+    <>
+      {!(iconType == "icon-only") ? (
         <button
-            type={type}
-            disabled={disabled || isLoading}
-            onClick={onClick}
-            className={`${className} w-fit flex justify-center font-medium items-center gap-2 px-9 py-2 rounded-full 
+          type={type}
+          disabled={disabled || isLoading}
+          onClick={onClick}
+          className={`${className} w-fit flex justify-center font-medium items-center gap-2 px-9 py-2 rounded-full 
                         disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-300
                         ${getColors()}`}
         >
-            {isLoading && iconType === "icon-left" ? <Loader2 className={`${iconStyle} animate-spin`} /> : (iconType === "icon-left" && Icon  && <Icon />)}
+          {isLoading && iconType === "icon-left" ? (
+            <Loader2 size={iconSize} className={`${iconStyle} animate-spin`} />
+          ) : (
+            iconType === "icon-left" && Icon && <Icon size={iconSize} />
+          )}
 
-            {children}
+          {children}
 
-            {isLoading && iconType === "icon-right"  ? <Loader2 className={`${iconStyle} animate-spin`} /> : (iconType === "icon-right" && Icon  && <Icon />)}
+          {isLoading && iconType === "icon-right" ? (
+            <Loader2 size={iconSize} className={`${iconStyle} animate-spin`} />
+          ) : (
+            iconType === "icon-right" && Icon && <Icon size={iconSize} />
+          )}
         </button>
-        :
+      ) : (
         <button
-            type={type}
-            disabled={disabled}
-            onClick={onClick}
-            className={`${className} size-12 cursor-pointer flex justify-center items-center  rounded-full  ${getColors()}`}
+          type={type}
+          disabled={disabled}
+          onClick={onClick}
+          className={`${className} size-12 cursor-pointer flex justify-center items-center  rounded-full  ${getColors()}`}
         >
-              {Icon && <Icon className={iconStyle} />}  
-        </button>}
-        </>
-    )
-}
+          {Icon && <Icon size={iconSize} className={iconStyle} />}
+        </button>
+      )}
+    </>
+  );
+};
 
-export default Button
+export default Button;
