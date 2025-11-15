44f12427f1f03a50a9b7b1febe91a8826e106b5b
JoelAdj01 <kofienergy15@gmail.com>
2025-11-15 22:35:18 +0000

    Home page update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,6 +6,11 @@ import vid1 from './imgs/vid-01.mp4';
 import vid2 from './imgs/vid-02.mp4';
 import vendorIllustration from './imgs/vendor-illus.png'
 import item1 from './imgs/item-01.jpg';
+import iphone from "./imgs/iphone.png"
+import sneaker from "./imgs/Sneaker.png"
+import cosmetics from "./imgs/cusmetics.png"
+import shoe from "./imgs/Jordan Wallpaper Air Jordan 1 Black Grey.jpg"
+
 
 export const images = {
     img1,
@@ -13,7 +18,11 @@ export const images = {
     img3,
     logo,
     vendorIllustration,
-    item1
+    item1,
+    iphone,
+    sneaker,
+    cosmetics,
+    shoe
 };
 
 export const videos = {
